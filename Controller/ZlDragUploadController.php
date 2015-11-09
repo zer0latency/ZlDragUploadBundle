ad845fa6d9eeffa1827b1fee6eac5e501c88a65f
Daniil Kolesnik <kolesnik-dv@1mbank.ru>
2015-11-09 22:48:06 +0300

    Upgraded controller

--- Controller/ZlDragUploadController.php
+++ Controller/ZlDragUploadController.php
@@ -4,6 +4,7 @@ namespace zer0latency\ZlDragUploadBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -14,6 +15,17 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class ZlDragUploadController extends Controller
 {
+
+    /**
+     * @var string
+     */
+    protected $sessionId = '';
+
+    /**
+     * @var string
+     */
+    protected $resultPath = '';
+
     /**
      * @Route("zl_dragupload/file", name="zl_dragupload_file")
      * @Method({"POST"})
@@ -21,8 +33,14 @@ class ZlDragUploadController extends Controller
      */
     public function fileAction(Request $request)
     {
+        $this->sessionId = $request->getSession()->getId();
         $response = new JsonResponse();
-        $response->setData($request->files->get('file'));
+        $this->moveUploaded($request->files->get('file'));
+
+
+        $this->get('logger')->info('File uploaded', array(
+            'path' => $this->resultPath,
+        ));
 
         return $response;
     }
@@ -30,5 +48,12 @@ class ZlDragUploadController extends Controller
     protected function getUploadDir()
     {
         return '/tmp/';
+    }
+
+    protected function moveUploaded(UploadedFile $file)
+    {
+        $newName = $this->sessionId.'_'.$file->getClientOriginalName();
+        $newFile = $file->move($this->getUploadDir(), $newName);
+        $this->resultPath = $newFile->getPathname();
     }
 }
